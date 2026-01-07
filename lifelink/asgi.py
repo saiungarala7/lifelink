e8770c9059e01d9c219627f0daa7dcfccd3c1adf
saiungarala7 <venkatungarala2005@gmail.com>
2026-01-08 04:03:09 +0530

    Initial commit

---
+++ lifelink/asgi.py
@@ -0,0 +1,18 @@
+import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lifelink.settings')
+
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            __import__("chat.routing").routing.websocket_urlpatterns
+        )
+    ),
+})
+
